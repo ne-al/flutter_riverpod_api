82545e69d02530068bee35100b3fa729e030e2b7
Neal <priboi78046@gmail.com>
2024-02-16 12:37:20 +0530

    Fixed - Loading animation on comment icon

--- lib/app/screens/pages/home.dart
+++ lib/app/screens/pages/home.dart
@@ -3,6 +3,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_riverpod_api/app/widgets/post_card_widget.dart';
 import 'package:flutter_riverpod_api/core/models/post_model.dart';
 import 'package:flutter_riverpod_api/core/providers/api_provider.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class HomePage extends ConsumerWidget {
   const HomePage({super.key});
@@ -10,35 +11,35 @@ class HomePage extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     return Scaffold(
-      body: RefreshIndicator(
-        onRefresh: () {
-          return ref.refresh(fetchAllPost.future);
-        },
-        child: SafeArea(
-          child: ref.watch(fetchAllPost).when(
-            data: (data) {
-              return ListView.builder(
-                itemCount: data.length,
-                itemBuilder: (context, index) {
-                  PostModel modelData = PostModel.fromJson(data[index]);
-                  return PostCardWidget(
-                    postModel: modelData,
-                  );
-                },
-              );
-            },
-            error: (error, stackTrace) {
-              return Center(
-                child: Text(error.toString()),
-              );
-            },
-            loading: () {
-              return const Center(
-                child: CircularProgressIndicator.adaptive(),
-              );
-            },
-          ),
+      appBar: AppBar(
+        title: Text(
+          'Home Page',
+          style: GoogleFonts.oswald(),
         ),
+        centerTitle: true,
+      ),
+      body: ref.watch(fetchAllPost).when(
+        data: (data) {
+          return ListView.builder(
+            itemCount: data.length,
+            itemBuilder: (context, index) {
+              PostModel modelData = PostModel.fromJson(data[index]);
+              return PostCardWidget(
+                postModel: modelData,
+              );
+            },
+          );
+        },
+        error: (error, stackTrace) {
+          return Center(
+            child: Text(error.toString()),
+          );
+        },
+        loading: () {
+          return const Center(
+            child: CircularProgressIndicator.adaptive(),
+          );
+        },
       ),
     );
   }
--- lib/app/widgets/comment_widget.dart
+++ lib/app/widgets/comment_widget.dart
@@ -12,76 +12,69 @@ class CommentWidget extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return ref.watch(fetchPostComments(postId)).when(
-          data: (data) {
-            WoltModalSheetPage commentPage() => WoltModalSheetPage(
-                  isTopBarLayerAlwaysVisible: true,
-                  pageTitle: Text(
-                    'Comments',
-                    style: GoogleFonts.oswald(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 24,
-                    ),
-                  ),
-                  child: ListView.separated(
-                    shrinkWrap: true,
-                    physics: const NeverScrollableScrollPhysics(),
-                    separatorBuilder: (context, index) => const Divider(),
-                    itemCount: data.length,
-                    itemBuilder: (context, index) {
-                      CommentsModel commentsModel =
-                          CommentsModel.fromJson(data[index]);
+    final commentRef = ref.watch(fetchPostComments(postId));
 
-                      return Card(
-                        child: Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text(
-                                commentsModel.email,
-                                style: GoogleFonts.lato(
-                                  fontSize: 18,
-                                  fontWeight: FontWeight.w700,
-                                  letterSpacing: 0.6,
-                                ),
-                              ),
-                              const Gap(4),
-                              Text(
-                                commentsModel.body,
-                                textAlign: TextAlign.left,
-                                style: GoogleFonts.lato(
-                                  fontSize: 15,
-                                  fontWeight: FontWeight.w500,
-                                  letterSpacing: 0.4,
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
-                      );
-                    },
-                  ),
-                );
-            return IconButton(
-              onPressed: () => WoltModalSheet.show<void>(
-                context: context,
-                useSafeArea: true,
-                pageListBuilder: (context) {
-                  return [
-                    commentPage(),
-                  ];
-                },
-              ),
-              icon: const Icon(Icons.message_rounded),
-            );
-          },
-          error: (error, stackTrace) => Center(
-            child: Text('$error'),
+    WoltModalSheetPage commentPage(List<dynamic> data) => WoltModalSheetPage(
+          isTopBarLayerAlwaysVisible: true,
+          pageTitle: Text(
+            'Comments',
+            style: GoogleFonts.oswald(
+              fontWeight: FontWeight.bold,
+              fontSize: 24,
+            ),
           ),
-          loading: () => const Center(
-            child: CircularProgressIndicator.adaptive(),
+          child: ListView.separated(
+            shrinkWrap: true,
+            physics: const NeverScrollableScrollPhysics(),
+            separatorBuilder: (context, index) => const Divider(),
+            itemCount: data.length,
+            itemBuilder: (context, index) {
+              CommentsModel commentsModel = CommentsModel.fromJson(data[index]);
+
+              return Card(
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        commentsModel.email,
+                        style: GoogleFonts.lato(
+                          fontSize: 18,
+                          fontWeight: FontWeight.w700,
+                          letterSpacing: 0.6,
+                        ),
+                      ),
+                      const Gap(4),
+                      Text(
+                        commentsModel.body,
+                        textAlign: TextAlign.left,
+                        style: GoogleFonts.lato(
+                          fontSize: 15,
+                          fontWeight: FontWeight.w500,
+                          letterSpacing: 0.4,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              );
+            },
           ),
         );
+
+    return IconButton(
+      onPressed: () => commentRef.whenData(
+        (data) {
+          return WoltModalSheet.show(
+            context: context,
+            pageListBuilder: (context) => [
+              commentPage(data),
+            ],
+          );
+        },
+      ),
+      icon: const Icon(Icons.message_rounded),
+    );
   }
 }
